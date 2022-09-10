2cccc005c9498a37986c37778332b8cadc11c05a
reizen-desu <nokiaq80@gmail.com>
2022-09-10 20:31:29 +0200

    Add individual delete on question endpoint

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -102,15 +102,23 @@ def create_app(test_config=None):
         })
 
     '''
-  @TODO:
+  # !DONE:
   Create an endpoint to DELETE question using a question ID.
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page.
+  # ! MISSING: Add exception
   '''
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
-        return 0
+
+        question = Question.query.get(question_id)
+        question.delete()
+
+        return jsonify({
+            'success': True,
+            'deleted': question_id
+        })
 
     '''
   @TODO:
