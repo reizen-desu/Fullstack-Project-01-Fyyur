6f5508103a7621cad11dc33b727d77a345c158ad
reizen-desu <nokiaq80@gmail.com>
2022-09-16 18:53:44 +0200

    Add exception to delete_question endpoint

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -107,11 +107,10 @@ def create_app(test_config=None):
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page.
-  # ! MISSING: Add exception
   '''
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
-
+        try:
         question = Question.query.get(question_id)
         question.delete()
 
@@ -119,7 +118,8 @@ def create_app(test_config=None):
             'success': True,
             'deleted': question_id
         })
-
+        except:
+            abort(422)
     '''
   @TODO:
   Create an endpoint to POST a new question,
