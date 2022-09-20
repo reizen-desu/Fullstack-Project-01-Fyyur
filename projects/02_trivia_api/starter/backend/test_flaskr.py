09066008d5142f5d17bf6ab27355cb627e532b50
reizen-desu <nokiaq80@gmail.com>
2022-09-21 01:09:02 +0200

    Add paginated questions unit test

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -21,7 +21,7 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            DATABASE_USERNAME, DATABASE_PASSWORD, "localhost:5432", database_name)
+            DATABASE_USERNAME, DATABASE_PASSWORD, "localhost:5432", "trivia_test")
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -36,10 +36,21 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
+    IN-PROGRESS
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(data['success'], True)
+
+    def test_404_sent_requesting_beyond_valid_page(self):
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
