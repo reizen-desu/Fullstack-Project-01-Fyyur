561af626afdc0affcd37e673ccf28f2763233a11
reizen-desu <nokiaq80@gmail.com>
2022-09-20 04:38:58 +0200

    Docs: Clean up comments

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -60,21 +60,8 @@ def create_app(test_config=None):
             'categories': categories_dict
         })
 
-    '''
-  # ! DONE:
-  Create an endpoint to handle GET requests for questions,
-  including pagination (every 10 questions).
-  This endpoint should return a list of questions,
-  number of total questions, current category, categories.
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions.
-  '''
-
+    # This endpoint returns a list of questions and its adta
     @app.route('/questions')
-    # @cross_origin()
     def get_questions():
 
         # Used for pagination | get all questions and paginate
@@ -98,13 +85,7 @@ def create_app(test_config=None):
             'categories': categories_dict
         })
 
-    '''
-  # !DONE:
-  Create an endpoint to DELETE question using a question ID.
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page.
-  '''
+    # Deletes a question by question ID
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
         try:
@@ -117,16 +98,8 @@ def create_app(test_config=None):
             })
         except:
             abort(422)
-    '''
-  @DONE:
-  Create an endpoint to POST a new question,
-  which will require the question and answer text,
-  category, and difficulty score.
 
-  TEST: When you submit a question on the "Add" tab,
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.
-  '''
+    # Creates a new question with the data that the user enters
     @app.route('/questions', methods=['POST'])
     def post_question():
         body = request.get_json()
@@ -147,17 +120,7 @@ def create_app(test_config=None):
         except:
             abort(422)
 
-    '''
-  @DONE:
-  Create a POST endpoint to get questions based on a search term.
-  It should return any questions for whom the search term
-  is a substring of the question.
-
-  TEST: Search by any phrase. The questions list will update to include
-  only question that include that string within their question.
-  Try using the word "title" to start.
-  '''
-
+    # Returns a list of questions based in the search term
     @app.route('/questions/search', methods=['POST'])
     def search_questions():
         body = request.get_json()
@@ -173,15 +136,8 @@ def create_app(test_config=None):
                 'total_questions': len(search_results),
             })
         abort(404)
-    '''
-  #   ! DONE:
-  Create a GET endpoint to get questions based on category.
 
-  TEST: In the "List" tab / main screen, clicking on one of the
-  categories in the left column will cause only questions of that
-  category to be shown.
-  '''
-
+    # Get list of questions based on a given category
     @app.route('/categories/<int:category_id>/questions')
     def get_questions_by_category(category_id):
 
@@ -197,18 +153,6 @@ def create_app(test_config=None):
             'current_category': category_id
 
         })
-
-    '''
-  @DONE:
-  Create a POST endpoint to get questions to play the quiz.
-  This endpoint should take category and previous question parameters
-  and return a random questions within the given category,
-  if provided, and that is not one of the previous questions.
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not.
-  '''
 
     @app.route('/quizzes', methods=['POST'])
     def get_questions_for_quiz():
@@ -246,9 +190,7 @@ def create_app(test_config=None):
             abort(400)
 
     '''
-  #  !DONE:
-  Create error handlers for all expected errors
-  including 404 and 422.
+    ERROR HANDLERS
   '''
 
     @app.errorhandler(400)
