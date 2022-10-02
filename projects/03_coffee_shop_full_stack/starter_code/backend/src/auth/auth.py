bf4cb41bae73a997ff0839c156e5f1c6e4127036
reizen-desu <nokiaq80@gmail.com>
2022-10-02 18:34:28 +0200

    Add token auth header

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -35,7 +35,38 @@ class AuthError(Exception):
 
 
 def get_token_auth_header():
-    raise Exception('Not Implemented')
+    # raise Exception('Not Implemented')
+
+    auth_header = request.headers.get('Authorization', None)
+
+    if not auth_header:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    header_parts = auth_header.split(' ')
+
+    if header_parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer"'
+        }, 401)
+
+    elif len(header_parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found'
+        }, 401)
+
+    elif len(header_parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token'
+        }, 401)
+
+    token = header_parts[1]
+    return token
 
 
 '''
