d4a2bdf8874a96ac6956f0a242a2f25b93ff3a6c
reizen-desu <nokiaq80@gmail.com>
2022-10-02 18:42:59 +0200

    Update frontend environment details

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* @TODO replace with your variables
+/* @DONE replace with your variables
  * ensure all variables on this page match your project
  */
 
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-w83e009q.us', // the auth0 domain prefix
+    audience: 'https://localhost:5000', // the audience set for the auth0 app
+    clientId: 'X5bU0An6GS9epNRHN7OUSayz1QhklFBv', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
